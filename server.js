30def8d8c9fa9c64f9461d029ab07ab234b9ea80
JonasMota <jonasmotafeitosa@gmail.com>
2022-03-14 15:41:30 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const cors = require('cors')
+const express = require ('express')
+const server = express()
+const axios = require('axios')
+
+ server.use(cors())
+
+server.get('/', async(req, res)=>{
+
+    try {
+        const { data } = await axios ('https://jsonplaceholder.typicode.com/users')
+
+    return res.json(data)
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+})
+
+
+
+server.listen(8081, ()=>{
+    console.log("servidor rodando http://localhost:8081")
+})
+
